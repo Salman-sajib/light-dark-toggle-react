426174fe73affdb19223287ea405fbc2ac155f90
Salman-sajib <sfsajib@gmail.com>
2024-03-14 14:09:02 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import LightDarkToggle from './components/LightDarkToggle';
+
+function App() {
+  return (
+    <>
+      <LightDarkToggle />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/LightDarkToggle.jsx
@@ -0,0 +1,22 @@
+import useLocalStorage from '../customHooks/useLocalStorage';
+
+function LightDarkToggle() {
+  const [theme, setTheme] = useLocalStorage('theme', 'dark');
+
+  function handleThemeToggle() {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  }
+
+  // console.log(theme);
+
+  return (
+    <div className='light-dark-mode' data-theme={theme}>
+      <div className='container'>
+        <p>Hello World</p>
+        <button onClick={handleThemeToggle}>Change Theme</button>
+      </div>
+    </div>
+  );
+}
+
+export default LightDarkToggle;
